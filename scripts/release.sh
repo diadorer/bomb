88092d402d14aa9b9ef0dba38ecdbeb441aa953f
Nikita Barinov <diadorer@yandex.ru>
2021-09-09 15:46:26 +0300

    :bomb: release 1.24.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-read -p "Are you sure? " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
-fi
-
 REPO='https://github.com/diadorer/bomb'
 
 GREEN=`tput setaf 2`
+YELLOW=`tput setaf 3`
 BOLD=`tput bold`
 RESET=`tput sgr0`
+
+read -p "${BOLD}Do you really want to release${RESET} $(poetry version)? ${YELLOW}(y/д)${RESET}" -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[YyДд]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
 
 poetry version minor
 
