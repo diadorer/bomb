a53d24e4708f33dc5542d8e959feda90ffa274cf
Nikita Barinov <diadorer@yandex.ru>
2021-09-09 15:17:24 +0300

    :bomb: release 1.20.0

--- scripts/release.sh
+++ scripts/release.sh
@@ -11,9 +11,14 @@ poetry version minor
 version=$(poetry version --short)
 message=":bomb: release $version"
 
+git checkout -b release/$version
 git commit -am "$message"
+git push -u origin release/$version
+
+gh pr create --title "$message" --body "# :bomb: Release $version! \n Please visit $REPO/releases/edit/$version to describe release notes!"
 gh release create "$version" --title "$message" --notes ":TBD:" 
+gh pr view --web
 
 echo -e "\n${GREEN}${BOLD}Done!${RESET}"
-echo "${BOLD}Please add release notes — $REPO/releases/edit/$version${RESET}"
+# echo "${BOLD}Please add release notes — $REPO/releases/edit/$version${RESET}"
 
